873234e0083c37c480b9d9c99c18216cf90f8e5e
Volodymyr <vovdata@gmail.com>
2023-01-28 15:43:45 +0100

    FormikNew1
    Form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
-import { LoginForm } from "./LoginForm/LoginForm";
+import { ProductReviewForm } from "./ProductReviewForm/ProductReviewForm";
+// import { LoginForm } from "./LoginForm/LoginForm";
 
 export class App extends Component {
   render() {
     return (
-      <LoginForm/>
+      // <LoginForm/>
+      <ProductReviewForm/>
     )
   }
 }
